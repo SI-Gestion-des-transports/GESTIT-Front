acf09dc759c79501af3192b8684197c61dae1468
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2023-09-05 00:57:06 +0200

    Mise  jour de la navigation dans l'interface

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,4 +8,61 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 
+  covoiturageChoicies = [
+    {
+      id: 1,
+      name: "Participer à un covoiturage"
+    },
+    {
+      id: 2,
+      name: "Organiser un covoiturage"
+    }
+  ]
+
+  ReservationsVehiculeServiceChoicies = [
+    {
+      id: 1,
+      name: "Mes réservations"
+    },
+    {
+      id: 2,
+      name: "Réserver un véhicule"
+    }
+  ]
+
+
+
+  constructor(private router: Router) { }
+
+  getCovoiturageChoice(event: any) {
+    switch (event.target.value) {
+      case '1':
+        this.router.navigateByUrl('covoiturages');
+        break;
+      case '2': console.log(2);
+        this.router.navigateByUrl('covoituragesOrganises');
+        break;
+      default: throw new Error();
+    }
+  }
+
+  getVehiculeServiceChoice(event: any) {
+    switch (event.target.value) {
+      case '1':
+        this.router.navigateByUrl('vehiculeService');
+        break;
+      case '2': console.log(2);
+        this.router.navigateByUrl('reservationsvs');
+        break;
+      default: throw new Error();
+    }
+  }
+
+  
+
+
+
+  // this.router.navigateByUrl('covoiturages');
 }
+
+
