dbff509c213cd4e7bd65d21e5c4bf7aa799cd59c
D-Azria <d.azria.pro@gmail.com>
2023-09-11 13:24:43 +0200

    Covoit Get Back

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,6 @@ export const environment = {
     utilisateurs: 'http://localhost:4200/utilisateur',
 
     users:'http://localhost:3000/users',
-    covoiturages: 'http://localhost:3000/covoiturages',
     covoiturage_reservationOK: 'http://localhost:3000/covoiturages/reservationEffectuee',
     reservationsvs:'http://localhost:3000/reservationsvs',
     adresses: 'http://localhost:3000/adresses',
@@ -15,9 +14,8 @@ export const environment = {
     categorie:'http://localhost:8080/categorie',
     motorisation:'http://localhost:8080/motorisation',
     statusVS:'http://localhost:8080/statusVS',
-
+    covoiturages: 'http://localhost:8080/covoiturages',
     vehiculeperso: 'http://localhost:8080/vehiculeperso',
-
     reservation: 'http://localhost:8080/reservation',
     covoituragesReserves: 'http://localhost:8080/covoituragesReserves',
     login: 'http://localhost:8080/login',
