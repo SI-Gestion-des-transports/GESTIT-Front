b8568989f01e83c7fd9042143aea5d77a5218e90
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2023-09-13 00:02:39 +0200

    MAJ

--- src/app/components/covoiturages/single-covoiturage/single-covoiturage.component.ts
+++ src/app/components/covoiturages/single-covoiturage/single-covoiturage.component.ts
@@ -10,6 +10,10 @@ import { UtilisateursService } from 'src/app/shared/services/utilisateurs.servic
 import { VehiculePersoService } from 'src/app/shared/services/vehicule.perso.service';
 import { environment } from 'src/environments/environment.development';
 
+/**
+ * Regroupe toutes les fonctions d'affichage  d'un covoiturage avec confirmation de réservation
+ * @author Atsuhiko Mochizuki
+ */
 
 @Component({
   selector: 'app-single-covoiturage',
@@ -18,19 +22,20 @@ import { environment } from 'src/environments/environment.development';
 })
 export class SingleCovoiturageComponent {
 
-  /*refection*/
-  mochizukiCovoiturage: Covoiturage | undefined;
+  covoiturageAconfirmer!: Covoiturage;
   title!: string;
   showDetailsInProgress!: boolean;
-  ObservableListePassagers: Observable<Utilisateur[]> | undefined;
-  mochizukiVehiculePerso: VehiculePerso | undefined;
+  organisateur!: Utilisateur;
+  observableListePassagers!: Observable<Utilisateur[]>;
+  vehiculeForCovoiturage!: VehiculePerso;
 
-
+  /*Injection des services*/
   constructor(private covoiturageService: CovoiturageService,
     private vehiculePersoService: VehiculePersoService,
     private utilisateurService: UtilisateursService,
     private router: Router,
     private route: ActivatedRoute) { }
+
 
   ngOnInit(): void {
     this.title = "Mon covoiturage";
@@ -38,30 +43,43 @@ export class SingleCovoiturageComponent {
 
     const covoiturageId = +this.route.snapshot.params['id'];  /*L'opérateur '+' effectue un cast*/
 
-    this.covoiturageService.getCovoiturageById(covoiturageId).subscribe((covoit) => {
-      /*récupération du covoiturage depuis son Observable*/
-      this.mochizukiCovoiturage = covoit;
+    this.covoiturageService.getCovoiturageById(covoiturageId)
+      .subscribe((covoit) => {
 
-      /*récupération d'un Observable de la liste des Passagers à partir de la liste des ids*/
-      this.ObservableListePassagers = this.getObservableOfPassengersList(covoit.passagersId);
+        /*récupération du covoiturage depuis son Observable*/
+        this.covoiturageAconfirmer = covoit;
 
-      /*Récupération du véhicule sur lequel s'opère le covoiturage*/
-      this.vehiculePersoService.findVpById_Mochizuki(covoit.vehiculePersoId.toString())
-        .subscribe(vehicule => this.mochizukiVehiculePerso = vehicule);
-    });
+        /*Récupération de l'organisateur*/
+        this.utilisateurService.findById(covoit.organisateurId)
+          .subscribe(user => this.organisateur = user);
+
+        /*récupération d'un Observable de la liste des Passagers à partir de la liste des ids*/
+        this.observableListePassagers = this.getObservableOfPassengersList(covoit.passagersId);
+
+        /*Récupération du véhicule sur lequel s'opère le covoiturage*/
+        this.vehiculePersoService.findVpById_Mochizuki(covoit.vehiculePersoId.toString())
+          .subscribe(vehicule => this.vehiculeForCovoiturage = vehicule);
+      });
   }
 
-
-
+  /**
+   * Récupère à partir d'un tableau d'identifiants passagers, les données correspondant à chaque
+   * utilisateur correspondant à l'id.
+   * La fonction combineLatest de rxjs permet de renvoyer un Observable du tableau d'utilisateur
+   * généré.
+   * Le contenu de ce dernier pourra donc être directement affiché dans le template à l'aide
+   * du pipe async.
+   * exemple : <div *ngFor="let passager of observableListePassagers | async">{{passager.nom}}</div>.
+   * 
+   * @param listIdPassagers 
+   * @returns 
+   */
   getObservableOfPassengersList(listIdPassagers: number[]): Observable<Utilisateur[]> {
     const arrayOfObservables = listIdPassagers.map(idPassager => {
       return this.utilisateurService.findById(idPassager);
     });
     return combineLatest(arrayOfObservables);
   }
-
-
-
 
   onShowDetails() {
     if (this.showDetailsInProgress) {
